ec73028d83c23bdfdcd65d8e7b0bec9fb4640d75
gsPatrick <patrickgsiqueira@hotmail.com>
2025-06-30 17:12:26 -0300

    mudanças

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,25 +7,25 @@ console.log('[services/index.js] File loaded. Starting service instantiation...'
 console.log('[services/index.js] Importing UserService class');
 const UserService = require('../features/user/user.service'); // Caminho corrigido assumindo src/user
 console.log('[services/index.js] Importing AccountService class');
-const AccountService = require('./features/account/account.service'); // Caminho corrigido assumindo src/account
+const AccountService = require('../features/account/account.service'); // Caminho corrigido assumindo src/account
 console.log('[services/index.js] Importing TransactionService class');
-const TransactionService = require('./features/transaction/transaction.service'); // Caminho corrigido assumindo src/transaction
+const TransactionService = require('../features/transaction/transaction.service'); // Caminho corrigido assumindo src/transaction
 console.log('[services/index.js] Importing InvoiceService class');
-const InvoiceService = require('./features/invoice/invoice.service'); // Caminho corrigido assumindo src/invoice
+const InvoiceService = require('../features/invoice/invoice.service'); // Caminho corrigido assumindo src/invoice
 console.log('[services/index.js] Importing CategoryService class');
-const CategoryService = require('./features/category/category.service'); // Caminho corrigido assumindo src/category
+const CategoryService = require('../features/category/category.service'); // Caminho corrigido assumindo src/category
 console.log('[services/index.js] Importing CalendarEventTypeService class');
-const CalendarEventTypeService = require('./features/calendarEventType/calendarEventType.service'); // Caminho corrigido assumindo src/calendarEventType
+const CalendarEventTypeService = require('../features/calendarEventType/calendarEventType.service'); // Caminho corrigido assumindo src/calendarEventType
 console.log('[services/index.js] Importing CalendarEventService class');
-const CalendarEventService = require('./features/calendarEvent/calendarEvent.service'); // Caminho corrigido assumindo src/calendarEvent
+const CalendarEventService = require('../features/calendarEvent/calendarEvent.service'); // Caminho corrigido assumindo src/calendarEvent
 console.log('[services/index.js] Importing InvestmentService class');
-const InvestmentService = require('./features/investment/investment.service'); // Caminho corrigido assumindo src/investment
+const InvestmentService = require('../features/investment/investment.service'); // Caminho corrigido assumindo src/investment
 console.log('[services/index.js] Importing AuthService class');
-const AuthService = require('./features/auth/auth.service'); // Caminho corrigido assumindo src/auth
+const AuthService = require('../features/auth/auth.service'); // Caminho corrigido assumindo src/auth
 
 // Crie instâncias dos serviços a partir das classes importadas
 // Exportamos diretamente um objeto com as instâncias para serem usadas em toda a aplicação.
-// Ao fazer "require('./features/./features/services')", você receberá este objeto com as instâncias prontas.
+// Ao fazer "require('../features/./features/services')", você receberá este objeto com as instâncias prontas.
 
 console.log('[services/index.js] Instantiating UserService');
 const userServiceInstance = new UserService();
