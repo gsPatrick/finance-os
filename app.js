6aaaafcbd5fdf1476ceed3d6278d2b62150bf929
gsPatrick <patrickgsiqueira@hotmail.com>
2025-07-01 05:27:31 -0300

    muitas mudanças -11

--- app.js
+++ app.js
@@ -4,36 +4,24 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
-// Caminhos relativos a partir de 'src/'
-const apiRoutes = require('./src/routes/index'); // Certifique-se de que este caminho está correto
-const errorMiddleware = require('./src/modules/errors/error.middleware'); // Corrigido: importa diretamente a função
-const ApiError = require('./src/modules/errors/apiError'); // Certifique-se de que este caminho está correto
+const apiRoutes = require('./src/routes/index');
+const errorMiddleware = require('./src/modules/errors/error.middleware');
+const ApiError = require('./src/modules/errors/apiError');
 
 const app = express();
 
-// Middlewares básicos
 app.use(helmet());
-
-// TRUST PROXY (apenas se estiver atrás de proxy em produção, ex: Heroku, Nginx)
-// Para testes locais, essa linha pode ser mantida sem impacto
 app.set('trust proxy', 1);
-
-// CORS liberado para tudo (para testes apenas)
 app.use(cors({ origin: '*' }));
-
-// Body parsers
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// ROTAS
-app.use('/', apiRoutes); // Ou use /api/... se preferir
+app.use('/', apiRoutes);
 
-// ROTA 404 (caso nenhuma rota seja encontrada)
 app.use((req, res, next) => {
   next(new ApiError(404, 'Rota não encontrada.'));
 });
 
-// MIDDLEWARE CENTRAL DE ERROS
 app.use(errorMiddleware);
 
 module.exports = app;
